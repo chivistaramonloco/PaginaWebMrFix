1659af045f4ac4251d7264e2aaf515dd7e1fc0bb
ramon <119838047+chivistaramonloco@users.noreply.github.com>
2022-12-08 02:43:12 +0000

    actualización

--- src/views/login.js
+++ src/views/login.js
@@ -223,8 +223,7 @@ class Login extends Component {
           <span id="msgIDU" class="color"></span>
           </FormGroup> 
           <FormGroup>
-          <input type="password"  name='Contrasena' id='Contrasena' placeholder="Contraseña" onChange={this.handleChange} value={this.state.form.Contrasena} />
-         
+          <input type="passworda" className="form-control"  name='Contrasena' id='Contrasena' placeholder="Contraseña" onChange={this.handleChange} value={this.state.form.Contrasena} />
           </FormGroup> 
           <span id="msgIDC" class="color"></span>
           <Button onClick={() => this.validaciones()}> Iniciar Sesión Administrador </Button>
